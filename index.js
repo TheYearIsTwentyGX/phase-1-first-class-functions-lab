a96eae957e5779513d7eac7fe9c15223731c82b7
Dylan Hays <twentygx@outlook.com>
2022-07-19 20:35:43 -0500

    commit tree b9b54c7fe782487e6c1aef1750a2f96c8087c432

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = (drivers) => drivers.slice(0,2);
+
+const returnLastTwoDrivers = (drivers) => drivers.slice(-2);
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(mult) { 
+    return (fare) => { return fare * mult; };
+}
+
+const fareDoubler = (fare) => createFareMultiplier(2)(fare);
+
+const fareTripler = (fare) => createFareMultiplier(3)(fare);
+
+function selectDifferentDrivers(arrayOfDrivers, func) {
+    return func(arrayOfDrivers);
+}
